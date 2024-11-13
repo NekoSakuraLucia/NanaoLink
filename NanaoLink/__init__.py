3811d91f50f2b368a6aa1832cf232bccd2ca4bce
NekoSakuraLucia <mokurolucia@gmail.com>
2024-11-13 16:26:36 +0700

    อัปเดต __init__

--- NanaoLink/__init__.py
+++ NanaoLink/__init__.py
@@ -1,3 +1,15 @@
+"""
+# ❤ NanaoLink
+
+แพ็กเกจ `client` สำหรับลาวาลิงก์ที่สืบทอดจาก `wavelink` ดั่งเดิมโค้ดและการใช้งานพื้นฐานยังคงเป็นของเขา\n
+ส่วนที่เรานำมาปรับเปลี่ยนบางอย่างคือการ ปรับแต่งให้มีความใช้งานง่ายขึ้น
+
+สิทธิทั้งหมดเราอนุญาตให้กับ MIT และ ถ้าหากเราพบเจอว่าคุณเอาไปขาย หรือ ทำการ renovate แล้วเอาไปขาย แล้ว แก้เครดิตเป็นของตัวเอง เราจะทำการลบ รีโพซิทอรี นี้ทันที\n
+We grant all rights to MIT, and if we find that you sell it, or renovate it and sell it, or change the credit to your own name, we will immediately remove this repository
+
+**Copyright &copy; 2024 NekoSakuraLucia (NekoMoew)**
+"""
+
 from .node import NodesCreate
 from .player import Nanao_Player
 
